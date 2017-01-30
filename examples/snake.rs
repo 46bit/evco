5c1ed46c2bed2bf99d3ac7e4a74bcf1a5ed2c4d7
Michael Mokrysz <hi@46b.it>
2017-01-30 00:50:25 +0000

    Major oneshot refactor of `Tree` API. #1
    * New method names, based upon:
      - Leaf and Branch https://en.wikipedia.org/wiki/Tree_(data_structure)#Terminology_used_in_Trees
      - Perfect vs Full https://en.wikipedia.org/wiki/Binary_tree#Types_of_binary_trees
    * `TreeVisitor` trait and `Tree::visit` method for doing things like counting nodes and depth.
    * Improved `TreeGen` modes - taking risk of naming confusion vs DEAP.
    * Removed `tree_with_env.rs` and `tree_with_mut_env.rs` 'examples.'

--- examples/snake.rs
+++ examples/snake.rs
@@ -3,7 +3,7 @@ extern crate jeepers;
 
 use rand::{OsRng, Rng, Rand};
 
-use jeepers::tree::{Tree, TreeGen};
+use jeepers::tree::{Tree, TreeGen, TreeVisitor};
 
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub enum TurnDirection {
@@ -141,60 +141,60 @@ pub enum SnakeTree {
     Move(TurnDirection),
 }
 
+use SnakeTree::*;
+
 impl<'a> Tree<'a> for SnakeTree {
     type Environment = &'a mut SnakeEnvironment;
     type Action = TurnDirection;
 
-    fn terminal_proportion<R: Rng>(_: &mut TreeGen<R>) -> f32 {
-        1.0 / (1.0 + 2.0)
+    fn branch<R: Rng>(tg: &mut TreeGen<R>, current_depth: usize) -> Self {
+        let direction = TurnDirection::rand(tg);
+        let true_ = Box::new(Self::child(tg, current_depth + 1));
+        let false_ = Box::new(Self::child(tg, current_depth + 1));
+        if tg.gen() {
+            IfDanger(direction, true_, false_)
+        } else {
+            IfFood(direction, true_, false_)
+        }
     }
 
-    fn rand_terminal<R: Rng>(tg: &mut TreeGen<R>, _: usize) -> SnakeTree {
-        SnakeTree::Move(TurnDirection::rand(tg))
-    }
-
-    fn rand_nonterminal<R: Rng>(tg: &mut TreeGen<R>, current_depth: usize) -> SnakeTree {
-        // A list of nonterminal construction methods.
-        let nonterminal_fs = [SnakeTree::rand_if_danger, SnakeTree::rand_if_food];
-        // Picks a random nonterminal constructor and runs it.
-        let nonterminal_f = tg.choose(&nonterminal_fs).unwrap();
-        nonterminal_f(tg, current_depth)
+    fn leaf<R: Rng>(tg: &mut TreeGen<R>, _: usize) -> Self {
+        Move(TurnDirection::rand(tg))
     }
 
     fn evaluate(&self, env: &mut SnakeEnvironment) -> Self::Action {
         match *self {
-            SnakeTree::IfDanger(direction, ref left_, ref right_) => {
+            IfDanger(direction, ref left_, ref right_) => {
                 if env.sense_danger(direction) {
                     left_.evaluate(env)
                 } else {
                     right_.evaluate(env)
                 }
             }
-            SnakeTree::IfFood(direction, ref left_, ref right_) => {
+            IfFood(direction, ref left_, ref right_) => {
                 if env.sense_food(direction) {
                     left_.evaluate(env)
                 } else {
                     right_.evaluate(env)
                 }
             }
-            SnakeTree::Move(direction) => direction,
+            Move(direction) => direction,
         }
     }
-}
 
-impl SnakeTree {
-    fn rand_if_danger<R: Rng>(tg: &mut TreeGen<R>, current_depth: usize) -> SnakeTree {
-        let direction = TurnDirection::rand(tg);
-        let true_ = SnakeTree::rand_node(tg, current_depth + 1);
-        let false_ = SnakeTree::rand_node(tg, current_depth + 1);
-        SnakeTree::IfDanger(direction, Box::new(true_), Box::new(false_))
-    }
-
-    fn rand_if_food<R: Rng>(tg: &mut TreeGen<R>, current_depth: usize) -> SnakeTree {
-        let direction = TurnDirection::rand(tg);
-        let true_ = SnakeTree::rand_node(tg, current_depth + 1);
-        let false_ = SnakeTree::rand_node(tg, current_depth + 1);
-        SnakeTree::IfFood(direction, Box::new(true_), Box::new(false_))
+    fn visit<'b, V>(&self, visitor: &mut V)
+        where V: TreeVisitor<'a, 'b, Self>,
+              Self: 'b
+    {
+        visitor.visit_node(self);
+        match *self {
+            IfDanger(_, ref left_, ref right_) |
+            IfFood(_, ref left_, ref right_) => {
+                left_.visit(visitor);
+                right_.visit(visitor);
+            }
+            _ => {}
+        }
     }
 }
 
@@ -205,6 +205,6 @@ fn main() {
     println!("{:?}", d);
 
     let mut tree_gen = TreeGen::full(&mut rng, 5, 10);
-    let p = SnakeTree::rand_tree(&mut tree_gen);
+    let p = SnakeTree::tree(&mut tree_gen);
     println!("{:?}", p);
 }
--- src/tree.rs
+++ src/tree.rs
@@ -1,9 +1,6 @@
 use rand::Rng;
 
 /// The Tree trait will be implemented by the trees of all Genetic Programs.
-///
-/// Generally only `rand_terminal` and `rand_nonterminal` will need redefining
-/// as other methods have default implementations.
 pub trait Tree<'a>
     where Self: Sized
 {
@@ -14,43 +11,43 @@ pub trait Tree<'a>
     type Action;
 
     /// Generate a new tree within the bounds specified by TreeGen.
-    fn rand_tree<R: Rng>(tg: &mut TreeGen<R>) -> Self {
-        Self::rand_node(tg, 0)
+    fn tree<R: Rng>(tg: &mut TreeGen<R>) -> Self {
+        Self::child(tg, 0)
     }
 
     /// Generate a random new node to go into a tree.
-    fn rand_node<R: Rng>(tg: &mut TreeGen<R>, current_depth: usize) -> Self {
-        let terminal_proportion = Self::terminal_proportion(tg);
-        if tg.condition(current_depth, terminal_proportion) {
-            Self::rand_terminal(tg, current_depth)
+    fn child<R: Rng>(tg: &mut TreeGen<R>, current_depth: usize) -> Self {
+        if tg.have_reached_a_leaf(current_depth) {
+            Self::leaf(tg, current_depth)
         } else {
-            Self::rand_nonterminal(tg, current_depth)
+            Self::branch(tg, current_depth)
         }
     }
 
-    /// Generate a Terminal node (a leaf) to go into a tree.
-    fn rand_terminal<R: Rng>(tg: &mut TreeGen<R>, current_depth: usize) -> Self;
+    /// Generate a branch node (a node with at least one Tree child).
+    fn branch<R: Rng>(tg: &mut TreeGen<R>, current_depth: usize) -> Self;
 
-    /// Generate a Non-Terminal node to go into a tree.
-    fn rand_nonterminal<R: Rng>(tg: &mut TreeGen<R>, current_depth: usize) -> Self;
+    /// Generate a leaf node (a node without any Tree children).
+    fn leaf<R: Rng>(tg: &mut TreeGen<R>, current_depth: usize) -> Self;
 
-    /// What proportion of possible tree nodes are terminals? 0.0 to 1.0.
-    /// TreeGen argument may be removed.
-    fn terminal_proportion<R: Rng>(tg: &mut TreeGen<R>) -> f32;
-
-    /// `evaluate` is called on the root node of a Tree to get its output.
+    /// Used to evaluate the root node of a Tree.
     fn evaluate(&self, env: Self::Environment) -> Self::Action;
+
+    /// For calling TreeVisitor::visit_node on every node of a Tree.
+    fn visit<'b, V>(&self, visitor: &mut V)
+        where V: TreeVisitor<'a, 'b, Self>,
+              Self: 'b;
 }
 
-/// Whether we're generating fully-balanced trees, or ones whose leaves are at
-/// varying lengths. Generally only useful inside Tree implementations - use
-/// `TreeGen::full`, `TreeGen::grow`, `TreeGen::half_and_half`.
+/// The tree generation mode in use. See `TreeGen`.
 #[derive(PartialEq, Eq, Clone, Copy, Debug)]
 pub enum TreeGenMode {
-    /// Generating a fully-balanced tree with Terminals at a fixed depth.
+    /// Corresponds to `TreeGen::perfect`.
+    Perfect(usize),
+    /// Corresponds to `TreeGen::full`.
     Full,
-    /// Generating a tree with Terminals (leaves) at varying depths.
-    Grow,
+    /// Corresponds to `TreeGen::full_ranged`.
+    FullRanged(usize),
 }
 
 /// Configure generation of trees. This manages tree depth by deciding when to
@@ -59,62 +56,93 @@ pub enum TreeGenMode {
 pub struct TreeGen<'a, R>
     where R: 'a + Rng
 {
-    /// A `rand::Rng` implementation for generating random tree nodes.
-    pub rng: &'a mut R,
     /// Which tree depth logic to use.
     pub mode: TreeGenMode,
+    /// A `rand::Rng` implementation for generating random tree nodes.
+    pub rng: &'a mut R,
     /// The minimum depth of trees to generate.
     pub min_depth: usize,
     /// The maximum depth of trees to generate.
     pub max_depth: usize,
-    /// Internal randomly-chosen height to make the tree.
-    pub chosen_depth: usize,
 }
 
 impl<'a, R> TreeGen<'a, R>
     where R: 'a + Rng
 {
-    /// Generate a fully-balanced tree between the depth bounds.
-    pub fn full(rng: &mut R, min_depth: usize, max_depth: usize) -> TreeGen<R> {
+    /// Generate a perfect tree. All leaves are at the same depth in the range
+    /// [min_depth, max_depth].
+    ///
+    /// **This is the equivalent of DEAP's `genFull`.**
+    pub fn perfect(rng: &mut R, min_depth: usize, max_depth: usize) -> TreeGen<R> {
         let chosen_depth = rng.gen_range(min_depth, max_depth + 1);
+        TreeGen {
+            rng: rng,
+            mode: TreeGenMode::Perfect(chosen_depth),
+            min_depth: min_depth,
+            max_depth: max_depth,
+        }
+    }
+
+    /// Generate a full tree, one with leaves at varying depths. Leaf depths are
+    /// linearly distributed between min_depth and a chosen depth in the range.
+    ///
+    /// **This is NOT the same as DEAP's `genFull`. See `TreeGen::full`**
+    pub fn full(rng: &mut R, min_depth: usize, max_depth: usize) -> TreeGen<R> {
         TreeGen {
             rng: rng,
             mode: TreeGenMode::Full,
             min_depth: min_depth,
             max_depth: max_depth,
-            chosen_depth: chosen_depth,
         }
     }
 
-    /// Generate a varying-depth tree between the depth bounds.
-    pub fn grow(rng: &mut R, min_depth: usize, max_depth: usize) -> TreeGen<R> {
+    /// Generate a full tree, one with leaves at varying depths. Leaf depths are
+    /// linearly distributed between min_depth and a chosen depth in the range.
+    ///
+    /// **This is the equivalent of DEAP's `genGrow`.**
+    pub fn full_ranged(rng: &mut R, min_depth: usize, max_depth: usize) -> TreeGen<R> {
         let chosen_depth = rng.gen_range(min_depth, max_depth + 1);
         TreeGen {
             rng: rng,
-            mode: TreeGenMode::Grow,
+            mode: TreeGenMode::FullRanged(chosen_depth),
             min_depth: min_depth,
             max_depth: max_depth,
-            chosen_depth: chosen_depth,
         }
     }
 
-    /// Randomly choose between a fully-balanced tree and a varying-depth tree.
+    /// Randomly choose between `TreeGen::perfect` and `TreeGen::full_ranged`.
+    ///
+    /// **This is the equivalent of DEAP's `genHalfAndHalf`.**
     // N.B. If TreeGen is ever Clone the random choice needs revising.
     pub fn half_and_half(rng: &mut R, min_depth: usize, max_depth: usize) -> TreeGen<R> {
         if rng.gen() {
-            Self::full(rng, min_depth, max_depth)
+            Self::perfect(rng, min_depth, max_depth)
         } else {
-            Self::grow(rng, min_depth, max_depth)
+            Self::full_ranged(rng, min_depth, max_depth)
         }
     }
 
-    /// Chooses whether to generate a Terminal (leaf) node. Used by `Tree::rand_node`.
-    pub fn condition(&mut self, current_depth: usize, term_prob: f32) -> bool {
+    /// Chooses whether to generate a Leaf node. Used by `Tree::child`.
+    pub fn have_reached_a_leaf(&mut self, current_depth: usize) -> bool {
         match self.mode {
-            TreeGenMode::Full => current_depth == self.chosen_depth,
-            TreeGenMode::Grow => {
-                (current_depth == self.chosen_depth) ||
-                (current_depth >= self.min_depth && self.next_f32() < term_prob)
+            TreeGenMode::Perfect(chosen_depth) => current_depth == chosen_depth,
+            TreeGenMode::Full => {
+                // This given an equal 1-in-depth_interval chance at every intermediary depth.
+                // Earlier checks ensure in the (1/depth)*(depth-1) case we reach chosen_depth,
+                // we do finally place a Leaf.
+                let depth_interval = self.max_depth - self.min_depth;
+                // @TODO: Avoid converting depth_interval.
+                current_depth == self.max_depth ||
+                (current_depth >= self.min_depth) && self.gen_weighted_bool(depth_interval as u32)
+            }
+            TreeGenMode::FullRanged(chosen_depth) => {
+                // This given an equal 1-in-depth_interval chance at every intermediary depth.
+                // Earlier checks ensure in the (1/depth)*(depth-1) case we reach chosen_depth,
+                // we do finally place a Leaf.
+                let depth_interval = chosen_depth - self.min_depth;
+                // @TODO: Avoid converting depth_interval.
+                current_depth == chosen_depth ||
+                (current_depth >= self.min_depth) && self.gen_weighted_bool(depth_interval as u32)
             }
         }
     }
@@ -135,4 +163,14 @@ impl<'a, R> Rng for TreeGen<'a, R>
     fn fill_bytes(&mut self, dest: &mut [u8]) {
         self.rng.fill_bytes(dest)
     }
+}
+
+/// For running a callback against every node of a tree. Use with `Tree::visit`.
+///
+/// Suggested uses: counting nodes in a tree, depth of a tree, etc.
+pub trait TreeVisitor<'a, 'b, T>
+    where T: 'b + Tree<'a>
+{
+    /// Callback to run with each node.
+    fn visit_node(&mut self, node: &T);
 }
